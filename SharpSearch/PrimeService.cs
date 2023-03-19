36de1c31f790c9e97fdee5de522ea4be015a2da1
tg2648 <45052145+tg2648@users.noreply.github.com>
2023-03-18 23:19:19 -0400

    Init skeleton with testing

---
+++ SharpSearch/PrimeService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Prime.Services;
+public class PrimeService
+{
+    public bool IsPrime(int candidate)
+    {
+        throw new NotImplementedException("Please create a test first.");
+    }
+}
