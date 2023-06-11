b7091868534eecba4c9a08a623d93bf9222e2b40
tg2648 <45052145+tg2648@users.noreply.github.com>
2023-06-11 08:48:36 -0400

    Read files from CLI

--- src/SharpSearch/Program.cs
+++ src/SharpSearch/Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            foreach (string line in File.ReadLines(args[0]))
+            {
+                Console.WriteLine(line);
+            }
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("File could not be read:");
+            Console.WriteLine(e.Message);
+        }
+    }
+}
