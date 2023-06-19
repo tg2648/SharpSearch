75d58063d527c8764781980a931e7ea31fbf93fe
tg2648 <45052145+tg2648@users.noreply.github.com>
2023-06-18 18:07:56 -0700

    Add CLI `add` command

--- src/SharpSearch/SharpSearch.cs
+++ src/SharpSearch/SharpSearch.cs
@@ -1,11 +1,30 @@
-﻿namespace SharpSearch;
+﻿using System.CommandLine;
+
+namespace SharpSearch;
 
 public class SharpSearch
 {
-    public static void Main(string[] args)
+    public static async Task<int> Main(string[] args)
     {
-        string path = args[0];
+        var addCommand = new Command("add", "Add files or directories to the index");
+        var pathsArgument = new Argument<string[]>(
+            name: "paths",
+            description: "At least one path of file or directory to add to the index.");
+        pathsArgument.Arity = ArgumentArity.OneOrMore;
+        addCommand.AddArgument(pathsArgument);
+
         Index index = new();
-        index.Add(path);
+
+        addCommand.SetHandler((paths) =>
+        {
+            foreach (var path in paths)
+            {
+                index.Add(path);
+            }
+        }, pathsArgument);
+
+        var rootCommand = new RootCommand("SharpSearch File Index");
+        rootCommand.AddCommand(addCommand);
+        return await rootCommand.InvokeAsync(args);
     }
 }
